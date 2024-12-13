116b95c4a26fd95b2e7c4478aa7fac1797c0ca84
Madhushan R.M.S <sadeepm20@gmail.com>
2024-12-14 00:02:01 +0530

    Update visualizer.py

--- turtlebot_ekf/turtlebot_ekf/visualizer.py
+++ turtlebot_ekf/turtlebot_ekf/visualizer.py
@@ -16,6 +16,11 @@ class PredictedPathSub(Node):
 
         self.predicted_path_sub = self.create_subscription(Float32MultiArray, "/ekf/predicted_state", self.callback, 10, callback_group=self.cb_group)
         self.predicted_path_pub = self.create_publisher(Path, "/ekf/predicted_path", 10)
+        self.predicted_odom_pub = self.create_publisher(Odometry, "/ekf/predicted_odom", 10)
+
+        self.predicted_odom = Odometry()
+        self.predicted_odom.header.frame_id = "odom"
+        self.predicted_odom.child_frame_id = "predicted_odom"
 
         self.predicted_path = Path()
         self.predicted_path.header.frame_id = "odom"
@@ -30,6 +35,15 @@ class PredictedPathSub(Node):
         x_t_ = msg.data
 
         pred_q  = quaternion_from_euler(0, 0, x_t_[2])
+
+        self.predicted_odom.header.stamp = self.get_clock().now().to_msg()
+        self.predicted_odom.pose.pose.position.x = x_t_[0]
+        self.predicted_odom.pose.pose.position.y = x_t_[1]
+        self.predicted_odom.pose.pose.orientation.x = pred_q[0]
+        self.predicted_odom.pose.pose.orientation.y = pred_q[1]
+        self.predicted_odom.pose.pose.orientation.z = pred_q[2]
+        self.predicted_odom.pose.pose.orientation.w = pred_q[3]
+        self.predicted_odom_pub.publish(self.predicted_odom)
 
         self.transfrom_stamped_.header.stamp  = self.get_clock().now().to_msg()
         self.transfrom_stamped_.transform.translation.x = x_t_[0]
@@ -61,6 +75,11 @@ class CorrectedPathSub(Node):
 
         self.corrected_path_sub = self.create_subscription(Float32MultiArray, "/ekf/corrected_state", self.callback, 10, callback_group=self.cb_group)
         self.corrected_path_pub = self.create_publisher(Path, "/ekf/corrected_path", 10)
+        self.corrected_odom_pub = self.create_publisher(Odometry, "/ekf/corrected_odom", 10)
+
+        self.corrected_odom = Odometry()
+        self.corrected_odom.header.frame_id = "odom"
+        self.corrected_odom.child_frame_id = "corrected_odom"
 
         self.corrected_path = Path()
         self.corrected_path.header.frame_id = "odom"
@@ -76,6 +95,16 @@ class CorrectedPathSub(Node):
         x_t = msg.data
 
         corr_q = quaternion_from_euler(0, 0, x_t[2])
+
+        self.corrected_odom.header.stamp = self.get_clock().now().to_msg()
+        self.corrected_odom.pose.pose.position.x = x_t[0]
+        self.corrected_odom.pose.pose.position.y = x_t[1]
+        self.corrected_odom.pose.pose.orientation.x = corr_q[0]
+        self.corrected_odom.pose.pose.orientation.y = corr_q[1]
+        self.corrected_odom.pose.pose.orientation.z = corr_q[2]
+        self.corrected_odom.pose.pose.orientation.w = corr_q[3]
+        self.corrected_odom_pub.publish(self.corrected_odom)
+
 
         self.transfrom_stamped.header.stamp  = self.get_clock().now().to_msg()
         self.transfrom_stamped.transform.translation.x = x_t[0]
@@ -107,6 +136,11 @@ class FusedPathSub(Node):
 
         self.fused_path_sub = self.create_subscription(Float32MultiArray, "/ekf/fused_state", self.callback, 10, callback_group=self.cb_group)
         self.fused_path_pub = self.create_publisher(Path, "/ekf/fused_path", 10)
+        self.fused_odom_pub = self.create_publisher(Odometry, "/ekf/fused_odom", 10)
+
+        self.fused_odom = Odometry()
+        self.fused_odom.header.frame_id = "odom"
+        self.fused_odom.child_frame_id = "fused_odom"
 
         self.fused_path = Path()
         self.fused_path.header.frame_id = "odom"
@@ -122,6 +156,15 @@ class FusedPathSub(Node):
         x_t = msg.data
 
         corr_q = quaternion_from_euler(0, 0, x_t[2])
+
+        self.fused_odom.header.stamp = self.get_clock().now().to_msg()
+        self.fused_odom.pose.pose.position.x = x_t[0]
+        self.fused_odom.pose.pose.position.y = x_t[1]
+        self.fused_odom.pose.pose.orientation.x = corr_q[0]
+        self.fused_odom.pose.pose.orientation.y = corr_q[1]
+        self.fused_odom.pose.pose.orientation.z = corr_q[2]
+        self.fused_odom.pose.pose.orientation.w = corr_q[3]
+        self.fused_odom_pub.publish(self.fused_odom)
 
         self.transfrom_stamped.header.stamp  = self.get_clock().now().to_msg()
         self.transfrom_stamped.transform.translation.x = x_t[0]
